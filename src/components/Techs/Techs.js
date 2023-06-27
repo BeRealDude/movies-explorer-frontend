5134bc6422382f62a063f05d7dfc923c29bf9fb5
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-06-27 18:08:54 +0300

    feat: techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,8 +1,24 @@
+import './Techs.css'
+
 function Techs() {
     return (
-      <>
-      <div id="techs">компонент с использованными технологиями</div>
-      </>
+      <section className="techs" id="techs">
+        <h2 className="techs__title">Технологии</h2>
+      <div className="techs__line"></div>
+      <div className='techs__container'>
+      <h3 className="techs__subTitle">7 технологий</h3>
+      <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <ul className="list-techs">
+        <li className="list-techs__item">HTML</li>
+        <li className="list-techs__item">CSS</li>
+        <li className="list-techs__item">JS</li>
+        <li className="list-techs__item">React</li>
+        <li className="list-techs__item">Git</li>
+        <li className="list-techs__item">Express.js</li>
+        <li className="list-techs__item">mongoDB</li>
+      </ul>
+      </div>
+      </section>
     );
   }
   
