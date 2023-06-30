66ed602ea7a851431acef67dc46e1c4d5902afd5
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-06-30 06:51:42 +0300

    feat: profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,11 +18,21 @@ function App() {
   const [movies, setMovies] = useState(moviesData);
 
   const location = useLocation();
+
+  // const [currentUser, setCurrentUser] = useState({
+  //   _id: '',
+  //   email: '',
+  //   name: '',
+  // });
   
   
 
   function handleLogin() {
     setLoggedIn(true);
+  }
+
+  function handleLoginOut() {
+    setLoggedIn(false);
   }
 
   const saveMovie = (newMovie) => {
@@ -41,13 +51,13 @@ function App() {
         <Route path="/" element={<Main loggedIn={loggedIn} />} />
         <Route path="/movies" element={<Movies movies={movies} onSaveMovie={saveMovie} />} />
         <Route path="/saved-movies" element={<SavedMovies movies={movies}/>} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile isLoggedIn={handleLoginOut} />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/signin" element={<Login isLoggedIn={handleLogin}/>} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
 
-      {location.pathname === "/signup" || location.pathname === "/signin" ? (
+      {location.pathname === "/signup" || location.pathname === "/signin" || location.pathname === "/profile" ? (
         ""
       ) : (
         <Footer />
